5e69e152fb8043cc0ffdf9c26a7d9061523b3d3e
gnaneshwar151120 <amlucky151120@gmail.com>
2021-06-05 16:19:11 +0530

    Javascript

---
+++ constant.js
@@ -0,0 +1,11 @@
+// constant key word is used to not change the variable value
+let radius=  5
+const pi = 3.14
+let area
+// we can change the variale radius here but not pi
+radius = 6
+ 
+// if i try to change const it gives error
+// pi = 4     
+ area = pi * radius * radius
+ console.log(area)
---
+++ variables.js
@@ -0,0 +1,19 @@
+console.log(2+2)
+num = 2+2
+console.log(num)
+
+//use let keyword to create variable
+ let num1 = 5
+ console.log(num1)
+
+ //for string
+ let user_name="gnan"
+ console.log(user_name)
+
+ /*let num1 = 3
+ console.log(num1)
+ This code gives error because its already declared*/
+  
+ //but thid code runs
+ num1 = 4
+ console.log(num1) 
