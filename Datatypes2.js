2f11046394f7f8430224a093e03f2b667f1ee673
gnaneshwar151120 <amlucky151120@gmail.com>
2021-06-07 21:10:08 +0530

    Javascript

---
+++ Datatypes2.js
@@ -0,0 +1,53 @@
+//Strings
+
+let user_name='Gnan'
+console.log(user_name)
+
+//string concatination
+let first_name='Gnaneshwar'
+let last_name='Reddy'
+full_name= first_name + " " + last_name
+console.log(full_name) 
+
+//colons is string
+let colons = 'Gnaneshwar "Reddy"'
+console.log(colons)
+
+//Escape character
+let BackSlash = "Gnaneshwar \"Reddy\"" //without using single colon in between u can use backslash 
+ console.log(BackSlash)
+
+ let new_line="Gnaneshwar \nReddy"     //"\n" is used to go into new line
+ console.log(new_line)
+
+ let tab = "Gnaneshwar \tReddy"       //"\t" is used for tab space
+ console.log(tab)
+
+ let vertical_tab="Gnaneshwar \vReddy"  //"\v" is used for vertical tab
+ console.log(vertical_tab)
+
+ let back_space="Gnaneshwar \bReddy" //"\b" is used for back space
+ console.log(back_space)
+
+
+
+ //Boolean
+ let bool = 5>6            //only gives either true or false
+ console.log(bool)
+ console.log(typeof bool)
+
+ //Null
+ let user = null   
+ console.log(user) 
+ console.log(typeof user)         //it may be consider as a bug or feature 
+
+ //undefined
+ let undefined
+ console.log(undefined)
+ console.log(typeof undefined)
+
+ let num=5
+ console.log(5/"nvjkf")
+ console.log(typeof (5/"nvjkf"))   //js has this kind of bugs which we need to know
+
+ 
