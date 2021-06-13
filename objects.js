a7bf9e975e9f9c7630d99cd4ec914e617ba6ac52
gnaneshwar151120 <amlucky151120@gmail.com>
2021-06-13 20:22:23 +0530

    Javascript

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ let obj={}  //{} is the way to declare objects
 console.log(obj);
 console.log(typeof obj);
 
-
+console.log("******************");
 //objects are generally key-value in javascript
 let exm={
     name:"Gnaneshwar",
@@ -15,7 +15,7 @@ console.log(exm);
 //fetching a particular value
 console.log(exm.name);     //method1
 console.log(exm['tech']);   //method2
-
+console.log("******************");
 /*which method to be used?
 it based on requirement
 but i choose '.' 
@@ -38,3 +38,34 @@ let dic={
 }
 console.log(dic[input]);   //if i use . it shows undefined
 
+console.log("******************");
+
+//complex object: object inside object
+let details={
+    name:'Gnaneshwar',
+    age:'20',
+    education:{
+        school:'SR edu centre',
+        Intermediate:"NJC",
+        College:"CU"
+    }
+}
+console.log(details);      //shows all properties
+console.log(details.education);  //shows prpoerties of education
+console.log(details.education.school); //fetching data inside a key
+console.log(details.education.school.length);//finding length of a value  note:space is also considered
+console.log("******************");
+//what happens when we try to fetch a value which doesnt there in key?
+//example:
+console.log(details.education.school1);   //we get ouput as undefined
+// console.log(details.education.school.length)             //we get eror in this case ....so to avoid error we use"?"
+
+//console.log(details.education.school1?.length);  //may get erroe in some versions 
+//soo im any use it better to use
+console.log("******************");
+
+//delete:
+delete details.education.College
+console.log(details);
+
+console.log("******************");
